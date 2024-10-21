1782d9173389f504469e7f0504e04614645f116b
xlyth1 <xalyth10@gmail.com>
2024-10-21 23:39:07 +0200

    grouped sql statements in separate files

--- src/main/resources/database/Statements.sql
+++ src/main/resources/database/Statements.sql
@@ -3,3 +3,13 @@ SELECT * FROM Words_Lists;
 SELECT * FROM (Words_Lists JOIN Words_List_Items
 ON Words_Lists.id = Words_List_Items.words_list_id)
 JOIN Words ON Words.id = Words_List_Items.word_id;
+
+SELECT Words.headword, Definitions.definition FROM
+(Words JOIN Words_Definitions
+ON Words.id = Words_Definitions.word_id) JOIN Definitions
+ON Words_Definitions.definition_id = Definitions.id;
+
+
+SELECT * FROM Words_Definitions GROUP BY word_id;
+
+DELETE FROM Words_Definitions WHERE id = 6;
