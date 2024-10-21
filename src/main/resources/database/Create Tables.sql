f7cdbd1b4e4417a1b707b2dab7e03bb765e1d8c3
xlyth1 <xalyth10@gmail.com>
2024-10-21 23:44:44 +0200

    deleted unneccesary UNIQUE constraints

--- src/main/resources/database/Create Tables.sql
+++ src/main/resources/database/Create Tables.sql
@@ -4,18 +4,18 @@ CREATE TABLE Words (
 );
 
 CREATE TABLE Definitions (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     definition TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE Words_Definitions (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     word_id INTEGER REFERENCES Words(id),
     definition_id INTEGER REFERENCES Definitions(id)
 );
 
 CREATE TABLE Words_Lists (
-    id INTEGER PRIMARY KEY UNIQUE,
+    id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
 );
 
