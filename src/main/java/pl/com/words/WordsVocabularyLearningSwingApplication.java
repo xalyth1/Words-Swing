944042ee0410add8afb6401d41a675eb6a6ad861
xlyth1 <xalyth10@gmail.com>
2024-12-10 13:59:05 +0100

    added words-service app to start locally if app in development testing mode

--- src/main/java/pl/com/words/WordsVocabularyLearningSwingApplication.java
+++ src/main/java/pl/com/words/WordsVocabularyLearningSwingApplication.java
@@ -5,6 +5,8 @@ import pl.com.words.gui.GUI;
 import pl.com.words.persistence.Database;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class WordsVocabularyLearningSwingApplication {
     public static void main(String[] args) {
@@ -17,6 +19,30 @@ public class WordsVocabularyLearningSwingApplication {
 
 
     private static void startLocalWordsServiceLocallyForTesting() {
-        // start Words Service on localhost
+        String jarPath = ConfigLoader.getInstance().getProperty("words.service.localAppName.path");
+        System.out.println(jarPath);
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath);
+        processBuilder.environment().put("SPRING_PROFILES_ACTIVE", "development");
+        Process process = null;
+        try {
+            process = processBuilder.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Words-Service-Local-Test instance application exited with code: " + exitCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (process != null && process.isAlive()) {
+                process.destroy();
+                System.out.println("Words-Service-Local-Test instance application process terminated.");
+            }
+        }
+
     }
 }
